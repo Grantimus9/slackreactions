d77cd7585edc253cdf405d62bb812473562f6e44
Grant Nelson <grantmnelsn@gmail.com>
2015-11-27 18:30:00 -0500

    Time to Add Slack Controllers

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,13 +1,17 @@
 class Reaction < ActiveRecord::Base
-
   # Carrierwave
   mount_uploader :image, ReactionUploader
 
   belongs_to :user
+  before_save :downcase_keywords
 
   # Rely on the Sunspot Solr gem/system to handle this
   searchable do
     text :keywords
+  end
+
+  def downcase_keywords
+    keywords.downcase
   end
 
 end
